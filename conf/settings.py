8ea9e408b859157c9305aec767d5db405ec1cda2
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-08-14 12:36:03 +0500

    set default database

--- conf/settings.py
+++ conf/settings.py
@@ -99,14 +99,19 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=config("DATABASE_URL", default="sqlite:///db.sqlite3"),
+#         conn_max_age=600,
+#         ssl_require=False
+#     )
+# }
 DATABASES = {
-    "default": dj_database_url.config(
-        default=config("DATABASE_URL", default="sqlite:///db.sqlite3"),
-        conn_max_age=600,
-        ssl_require=False
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
