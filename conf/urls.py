091b8cdc82b04986e882f1471c14591c19fcb69b
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-08-18 12:40:35 +0500

    add logo and fixed responsive view

--- conf/urls.py
+++ conf/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'logo/logo.png', permanent=True)),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
