caa4c762f4b82eb2e05fbbd1826a086e363283ae
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-08-24 22:42:36 +0500

    default pic and user profile and add the signup button

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class UserProfile(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.svg', blank=True)
     location = models.CharField(max_length=100, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     website = models.URLField(max_length=200, blank=True)
