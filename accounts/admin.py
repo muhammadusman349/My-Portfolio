86f25f61a0dad3921fc0f200ffa2249e005ecec0
muhammad usman <usmankhadim.uk.uk@gmail.com>
2025-09-01 06:43:54 +0500

    add birth_date field in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,12 +4,12 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'location')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'location', 'birth_date')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email', 'location')
     ordering = ('username',)
 
     fieldsets = UserAdmin.fieldsets + (
-        ('Profile Info', {'fields': ('bio', 'profile_picture', 'location')}),
+        ('Profile Info', {'fields': ('bio', 'profile_picture', 'location', 'birth_date')}),
         ('Social Links', {'fields': ('linkedin', 'github', 'website')}),
     )
